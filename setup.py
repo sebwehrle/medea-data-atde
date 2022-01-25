946449341443f18999b3c5893ae580d0265cc39d
Sebastian <sebastian.wehrle@boku.ac.at>
2022-01-25 13:25:18 +0100

    fix installation

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md', 'r', encoding='utf-8') as readme_file:
 
 setup(
     name='medea-data-atde',
-    version='0.1',
+    version='0.1.0',
     author='Sebastian Wehrle',
     author_email='sebastian.wehrle@boku.ac.at',
     description='data processing for the medea power system model',
     long_description=readme,
     long_description_content_type='text/markdown',
-    url='https://github.com/inwe-energy/medea',
+    url='https://github.com/sebwehrle/medea-data-atde',
     project_urls={},
     license='MIT',
     packages=find_packages(),
