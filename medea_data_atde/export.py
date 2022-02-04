3bf3e52efb377ef517f7cd89dbefa0335ca518b0
Sebastian <sebastian.wehrle@boku.ac.at>
2022-02-04 16:05:50 +0100

    include technology data

--- medea_data_atde/export.py
+++ medea_data_atde/export.py
@@ -4,11 +4,11 @@ import numpy as np
 from medea_data_atde.retrieve import hours_in_year
 
 
-def compile_symbols(tech_data, timeseries, zones, year, invest_conventionals=True, invest_renewables=True,
+def compile_symbols(root_dir, timeseries, zones, year, invest_conventionals=True, invest_renewables=True,
                     invest_storage=True, invest_tc=True):
     """
     prepares dictionaries used to build input data gdx-files for power system model medea
-    :param tech_data: path to data_static.xlsx
+    :param root_dir: root directory
     :param timeseries: path to timeseries_regional.csv
     :param zones: ISO 2-letter country code for countries to model. Default: ['AT', 'DE']
     :param year: integer of year to model. Default: 2016
@@ -19,12 +19,12 @@ def compile_symbols(tech_data, timeseries, zones, year, invest_conventionals=Tru
     :return:
     """
     idx = pd.IndexSlice
-
-    dict_technologies = {
-        'capacity': pd.read_excel(tech_data, 'Capacities', header=[0], index_col=[0, 1, 2], skiprows=[0, 1, 2]),
-        'capacity_transmission': pd.read_excel(tech_data, 'ATC', index_col=[0]),
-        'technology': pd.read_excel(tech_data, 'Technologies', header=[2], index_col=[2]).dropna(axis=0, how='all'),
-        'operating_region': pd.read_excel(tech_data, 'FEASIBLE_INPUT-OUTPUT', header=[0], index_col=[0, 1, 2]),
+    raw_data = root_dir / 'data' / 'raw'
+    technologies = {
+        'capacity': pd.read_csv(raw_data / 'capacities.csv'),
+        'capacity_transmission': pd.read_csv(raw_data / 'transmission.csv', index_col=[0, 1]),
+        'technology': pd.read_csv(raw_data / 'technologies.csv').dropna(axis=0, how='all'),
+        'operating_region': pd.read_csv(raw_data / 'operating_region.csv', index_col=[0, 1, 2]),
         'mappings': []
     }
 
@@ -33,31 +33,33 @@ def compile_symbols(tech_data, timeseries, zones, year, invest_conventionals=Tru
     }
 
     estimates = {
-        'ESTIMATES': pd.read_excel(tech_data, 'ESTIMATES', index_col=[0]),
-        'AIR_POLLUTION': pd.read_excel(tech_data, 'AIR_POLLUTION', index_col=[0]),
-        'CO2_INTENSITY': pd.read_excel(tech_data, 'CO2_INTENSITY', index_col=[0]),
-        'COST_TRANSPORT': pd.read_excel(tech_data, 'COST_TRANSPORT', index_col=[0]),
-        'DISCOUNT_RATE': pd.read_excel(tech_data, 'WACC', index_col=[0]),
-        'DISTANCE': pd.read_excel(tech_data, 'KM', index_col=[0])
+        ##        'ESTIMATES': pd.read_excel(tech_data, 'ESTIMATES', index_col=[0]),
+        'external_cost': pd.read_csv(raw_data / 'external_cost.csv', index_col=[0]),
+        #        'AIR_POLLUTION': pd.read_excel(tech_data, 'AIR_POLLUTION', index_col=[0]),
+        #        'CO2_INTENSITY': pd.read_excel(tech_data, 'CO2_INTENSITY', index_col=[0]),
+        #        'COST_TRANSPORT': pd.read_excel(tech_data, 'COST_TRANSPORT', index_col=[0]),
+        ##        'DISCOUNT_RATE': pd.read_excel(tech_data, 'WACC', index_col=[0]),
+        #        'DISTANCE': pd.read_excel(tech_data, 'KM', index_col=[0])
     }
 
     # create SETS
     # --------------------------------------------------------------------------- #
     sets = {
-        'e': {carrier: [True] for carrier in np.unique(dict_technologies['technology'][
-                                                           ['fuel', 'primary_product']])},  # all energy carriers
-        'i': {input: [True] for input in dict_technologies['technology']['primary_product'].unique()},  # energy inputs
-        'f': {final: [True] for final in dict_technologies['technology']['primary_product'].unique()},  # final energy
-        't': {tec: [True] for tec in dict_technologies['technology'].index.unique()},  # technologiest
-        'c': {chp: [True] for chp in dict_technologies['operating_region'].index.get_level_values(0).unique()},  # co-generation technologies
-        'd': {plant: [True] for plant in dict_technologies['technology'].loc[
-            dict_technologies['technology']['conventional'] == 1].index.unique()},  # dispatchable technologies
-        'r': {intmit: [True] for intmit in dict_technologies['technology'].loc[
-            dict_technologies['technology']['intermittent'] == 1].index.unique()},  # intermittent technologies
-        's': {storage: [True] for storage in dict_technologies['technology'].loc[
-            dict_technologies['technology']['storage'] == 1].index.unique()},  # storage technologies
-        'g': {transmit: [True] for transmit in dict_technologies['technology'].loc[
-            dict_technologies['technology']['transmission'] == 1].index.unique()},  # transmission technologies
+        'e': {carrier: [True] for carrier in np.unique(technologies['technology'][['fuel', 'primary_product']])},
+        # all energy carriers
+        'i': {input: [True] for input in technologies['technology']['primary_product'].unique()},  # energy inputs
+        'f': {final: [True] for final in technologies['technology']['primary_product'].unique()},  # final energy
+        't': {tec: [True] for tec in technologies['technology'].index.unique()},  # technologiest
+        'c': {chp: [True] for chp in technologies['operating_region'].index.get_level_values(0).unique()},
+        # co-generation technologies
+        'd': {plant: [True] for plant in technologies['technology'].loc[
+            technologies['technology']['conventional'] == 1].index.unique()},  # dispatchable technologies
+        'r': {intmit: [True] for intmit in technologies['technology'].loc[
+            technologies['technology']['intermittent'] == 1].index.unique()},  # intermittent technologies
+        's': {storage: [True] for storage in technologies['technology'].loc[
+            technologies['technology']['storage'] == 1].index.unique()},  # storage technologies
+        'g': {transmit: [True] for transmit in technologies['technology'].loc[
+            technologies['technology']['transmission'] == 1].index.unique()},  # transmission technologies
         'l': {f'l{x}': [True] for x in range(1, 5)},  # feasible operating regions
         'h': {f'h{hour}': [True] for hour in range(1, hours_in_year(year) + 1)},  # time steps / hours
         'z': {zone: [True] for zone in zones}  # market zones
@@ -73,18 +75,22 @@ def compile_symbols(tech_data, timeseries, zones, year, invest_conventionals=Tru
     # --------------------------------------------------------------------------- #
     # ** process technology data **
     # amend plant data by co-generation fuel need
-    dict_technologies['operating_region']['fuel_need'] = dict_technologies['operating_region']['fuel'] / \
-                                                         dict_technologies['technology'].loc[
-                                                             dict_technologies['technology'][
-                                                                 'heat_generation'] == 1, 'eta_ec']
+    technologies['operating_region']['fuel_need'] = technologies['operating_region']['fuel'] / \
+                                                    technologies['technology'].loc[
+                                                        technologies['technology'][
+                                                            'heat_generation'] == 1, 'eta_ec']
+    # transmission distances
+    technologies['distance'] = technologies['capacity_transmission']['distance'].loc[
+                               technologies['capacity_transmission']['distance'].index.get_level_values(0).str.contains(
+                                   '|'.join(zones)) &
+                               technologies['capacity_transmission']['distance'].index.get_level_values(1).str.contains(
+                                   '|'.join(zones)), :]
     # transmission capacities
-    dict_technologies.update({'capacity_transmission': dict_technologies['capacity_transmission'].loc[
-                                                           dict_technologies
-                                                               ['capacity_transmission'].index.str.contains(
-                                                               '|'.join(zones)),
-                                                           dict_technologies
-                                                               ['capacity_transmission'].columns.str.contains(
-                                                               '|'.join(zones))] / 1000})
+    technologies.update({'capacity_transmission': technologies['capacity_transmission']['ATC'].loc[
+                                                  technologies['capacity_transmission']['ATC'].index.get_level_values(
+                                                      0).str.contains('|'.join(zones)) &
+                                                  technologies['capacity_transmission']['ATC'].index.get_level_values(
+                                                      1).str.contains('|'.join(zones)), :] / 1000})
 
     # process time series data
     # --------------------------------------------------------------------------- #
@@ -126,15 +132,16 @@ def compile_symbols(tech_data, timeseries, zones, year, invest_conventionals=Tru
     for zone in zones:
         for fuel in model_prices:
             if fuel in estimates['COST_TRANSPORT'].index:
-                ts_data['price'][(fuel, zone)] = ts_data['timeseries'][fuel] + estimates['COST_TRANSPORT'].loc[fuel, zone]
+                ts_data['price'][(fuel, zone)] = ts_data['timeseries'][fuel] + estimates['COST_TRANSPORT'].loc[
+                    fuel, zone]
             else:
                 ts_data['price'][(fuel, zone)] = ts_data['timeseries'][fuel]
 
     # process INFLOWS to hydro storage plants
-    hydro_storage = dict_technologies['technology'].loc[(dict_technologies['technology']['storage'] == 1) &
-                                                        (dict_technologies['technology']['fuel'] == 'Water')].index
-    inflow_factor = dict_technologies['capacity'].loc[idx['Installed Capacity Out', :, year], hydro_storage].T / \
-                    dict_technologies['capacity'].loc[idx['Installed Capacity Out', :, year], hydro_storage].T.sum()
+    hydro_storage = technologies['technology'].loc[(technologies['technology']['storage'] == 1) &
+                                                   (technologies['technology']['fuel'] == 'Water')].index
+    inflow_factor = technologies['capacity'].loc[idx['Installed Capacity Out', :, year], hydro_storage].T / \
+                    technologies['capacity'].loc[idx['Installed Capacity Out', :, year], hydro_storage].T.sum()
     inflow_factor.columns = inflow_factor.columns.droplevel([0, 2])
     ts_inflows = pd.DataFrame(index=list(ts_data['ZONAL'].index),
                               columns=pd.MultiIndex.from_product([zones, sets['s'].index]))
@@ -147,10 +154,11 @@ def compile_symbols(tech_data, timeseries, zones, year, invest_conventionals=Tru
     # --------------------------------------------------------------------------- #
     # %% peak load and profiles
     # --------------------------------------------------------------------------- #
+    """
     estimates.update({'DISTANCE': estimates['DISTANCE'].loc[
         estimates['DISTANCE'].index.str.contains('|'.join(zones)),
         estimates['DISTANCE'].columns.str.contains('|'.join(zones))]})
-
+    """
     ts_data.update({'PEAK_LOAD': ts_data['ZONAL'].loc[:, idx[:, 'el', 'load']].max().unstack((1, 2)).squeeze()})
     peak_profile = ts_data['ZONAL'].loc[:, idx[:, :, 'profile']].max().unstack(2).drop('ror', axis=0, level=1)
     peak_profile.fillna(0, inplace=True)
@@ -174,39 +182,39 @@ def compile_symbols(tech_data, timeseries, zones, year, invest_conventionals=Tru
     }
 
     parameters = {
-        'AIR_POL_COST_FIX': estimates['AIR_POLLUTION']['fixed cost'],
-        'AIR_POL_COST_VAR': estimates['AIR_POLLUTION']['variable cost'],
-        'CAPACITY': dict_technologies['capacity'],
-        'CAPACITY_X': dict_technologies['capacity_transmission'],
-        'CAPACITY_STORAGE': dict_technologies['capacity'].loc
-            [idx['Storage Capacity', zones, year], sets['s'].index],
-        'CAPACITY_STORE_IN': dict_technologies['capacity'].loc
-            [idx['Installed Capacity In', zones, year], sets['s'].index],
-        'CAPACITY_STORE_OUT': dict_technologies['capacity'].loc
-            [idx['Installed Capacity Out', zones, year], sets['s'].index],
-        'CAPITALCOST': dict_technologies['technology'].loc[:, 'eqacapex_p'].round(4),
-        'CAPITALCOST_E': dict_technologies['technology'].loc[sets['s'].index, 'eqacapex_e'],
-        'CAPITALCOST_P': dict_technologies['technology'].loc[sets['s'].index, 'eqacapex_p'],
-        'CAPITALCOST_X':  dict_technologies['technology'].loc[sets['g'].index, 'eqacapex_p'],
-        'CO2_INTENSITY': estimates['CO2_INTENSITY'],
+        'AIR_POL_COST_FIX': estimates['external_cost']['fixed cost'].dropna(),
+        'AIR_POL_COST_VAR': estimates['external_cost']['variable cost'].dropna(),
+        'CAPACITY': technologies['capacity'],
+        'CAPACITY_X': technologies['capacity_transmission'],
+        'CAPACITY_STORAGE': technologies['capacity'].loc
+        [idx['Storage Capacity', zones, year], sets['s'].index],
+        'CAPACITY_STORE_IN': technologies['capacity'].loc
+        [idx['Installed Capacity In', zones, year], sets['s'].index],
+        'CAPACITY_STORE_OUT': technologies['capacity'].loc
+        [idx['Installed Capacity Out', zones, year], sets['s'].index],
+        'CAPITALCOST': technologies['technology'].loc[:, 'eqacapex_p'].round(4),
+        'CAPITALCOST_E': technologies['technology'].loc[sets['s'].index, 'eqacapex_e'],
+        'CAPITALCOST_P': technologies['technology'].loc[sets['s'].index, 'eqacapex_p'],
+        'CAPITALCOST_X': technologies['technology'].loc[sets['g'].index, 'eqacapex_p'],
+        'CO2_INTENSITY': estimates['external_cost']['CO2_intensity'].dropna(),
         # 'CONVERSION': dict_technologies['technology']['eta_ec'],
-        'COST_OM_QFIX': dict_technologies['technology']['opex_f'],
-        'COST_OM_VAR': dict_technologies['technology']['opex_v'],
+        'COST_OM_QFIX': technologies['technology']['opex_f'],
+        'COST_OM_VAR': technologies['technology']['opex_v'],
         'DEMAND': ts_data['ZONAL'].loc[:, idx[:, :, 'load']].stack((0, 1)).reorder_levels((1, 0, 2)).round(4),
         # 'DISCOUNT_RATE': [],
-        'DISTANCE': estimates['DISTANCE'].stack(),
-        'FEASIBLE_INPUT': dict_technologies['operating_region']['fuel_need'],
-        'FEASIBLE_OUTPUT': dict_technologies['operating_region'][['el', 'ht']].droplevel('f').stack(),
+        'DISTANCE': technologies['distance'],
+        'FEASIBLE_INPUT': technologies['operating_region']['fuel'],
+        'FEASIBLE_OUTPUT': technologies['operating_region'][['el', 'ht']].droplevel('f').stack(),
         'INFLOWS': ts_data['INFLOWS'].stack((0, 1)).reorder_levels((1, 0, 2)).astype('float').round(4),
         # 'LAMBDA': estimates['LAMBDA'],
-        'LIFETIME': dict_technologies['technology']['lifetime'],
+        'LIFETIME': technologies['technology']['lifetime'],
         # 'MAP_INPUTS': [],
         # 'MAP_OUTPUTS': [],
         'PEAK_LOAD': ts_data['PEAK_LOAD'],
         'PEAK_PROFILE': ts_data['PEAK_PROFILE'],
         'PRICE_CO2': ts_data['price'].loc[:, idx['EUA', :]].stack().reorder_levels((1, 0)),
         'PRICE': ts_data['price'].drop(['EUA', 'price_day_ahead'], axis=1).stack((0, 1)).reorder_levels
-            ((2, 0, 1)).round(4),  # TODO: performance warning
+        ((2, 0, 1)).round(4),  # TODO: performance warning
         # 'PRICE_TRADE': [],
         'PROFILE': ts_data['ZONAL'].loc[:, idx[:, :, 'profile']].stack((0, 1)).reorder_levels((1, 0, 2)).round(4),
         # 'SIGMA': estimates['SIGMA'],
